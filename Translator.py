9880a61fe1ae393783d3dbfadf0f08f0e4c31a3e
Protanton <protanton@gmail.com>
2021-12-12 17:57:25 +0300

    feat: added the function of translating a custom word to the desired one from the list, passed as the second argument

--- Translator.py
+++ Translator.py
@@ -32,3 +32,15 @@ with open(f'{word}.txt', mode='w', encoding='utf-8') as file:
 
             print(result, file=file, sep='\n', flush=True)
             print(result)
+    else:
+        url = f"https://context.reverso.net/translation/{language_from}-{languages_to}/{word}"
+        request = session.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+        soup = BeautifulSoup(request.text, 'html.parser')
+        translations = soup.select_one('#translations-content').select('a')
+        examples = soup.select_one('#examples-content').select('.example')
+        result = '\n'.join([languages_to + ' Translations:',
+                            str.strip(translations[0].text) + '\n',
+                            languages_to + ' Examples:',
+                            str.strip(examples[0].select('.text')[0].text) + ':',
+                            str.strip(examples[0].select('.text')[1].text) + '\n\n'])
+        print(result)
